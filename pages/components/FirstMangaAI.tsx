add77c0502e47d52a084880294639ad87490db41
FahadZaheerfzr <fahadzaheer19@gmail.com>
2023-09-05 00:09:58 +0500

    Mobile made responsive

--- pages/components/FirstMangaAI.tsx
+++ pages/components/FirstMangaAI.tsx
@@ -4,17 +4,17 @@ import { IoMdCopy } from 'react-icons/io'
 const FirstMangaAI = () => {
       return (
 
-            <div className='bg-main-gray pb-5 h-screen'>
+            <div className='bg-main-gray pb-5 h-[80vh] lg:h-screen'>
                   <div className='flex flex-col lg:flex-row lg:items-center bg-main-gray h-full font-Lato'>
-                        <div className=' lg:w-1/2 p-3 lg:ml-10 pt-5 font-Lato'>
-                              <h1 className='text-[#A075B4] leading-normal text-3xl lg:text-[68px] font-custom9 '>MANGA AI</h1>
-                              <h3 className='text-[#88C48A] font-custom8 text-lg lg:text-[39px]'>Unleash Your Anime Creativity </h3>
-                              <p className='text-[#F8F7F5] text-sm lg:text-2xl mt-16'>Welcome to Manga AI, where creativity meets technology to revolutionize anime content creation. Our platform blends AI and blockchain, offering a unique space for creators and fans to explore, collaborate, and thrive.</p>
+                        <div className='w-[85%] lg:w-1/2 p-3 ml-8 lg:ml-10 pt-[15%] lg:pt-5 font-Lato'>
+                              <h1 className='text-[#A075B4] leading-normal text-[44px] lg:text-[68px] font-custom9 '>MANGA AI</h1>
+                              <h3 className='text-[#88C48A] font-custom8 text-[23px] lg:text-[39px]'>Unleash Your Anime Creativity </h3>
+                              <p className='text-[#F8F7F5] text-xs lg:text-2xl mt-16'>Welcome to Manga AI, where creativity meets technology to revolutionize anime content creation. Our platform blends AI and blockchain, offering a unique space for creators and fans to explore, collaborate, and thrive.</p>
 
                               <div className='flex items-center gap-x-4 mt-5'>
-                                    <button className="rounded-md bg-gradient-to-r from-purple-500 to-green-500 text-[26px] text-[#F8F7F5] px-6 py-3">Generate Anime Art</button>
+                                    <button className="rounded-[13px] bg-gradient-to-r from-purple-500 to-green-500 text-sm lg:text-[26px] text-[#F8F7F5] px-6 py-3">Generate Anime Art</button>
 
-                                    <div className='w-[3px] h-[57.009px] ml-1 bg-[#8F41B4]' />
+                                    <div className='w-[1.5px] lg:w-[3px] h-[27px] lg:h-[57.009px] ml-1 bg-[#8F41B4]' />
 
                                     
                                     <p className='text-[#F8F7F5] text-sm lg:text-[26px] font-custom7 '><u>Documentation</u></p>
@@ -23,6 +23,7 @@ const FirstMangaAI = () => {
 
                         <div className='flex h-full items-center lg:w-[70%] justify-center relative'>
                               <img className='hidden lg:block' src='/images/hero-image.svg' />
+                              <img className='absolute lg:hidden' src='/images/hero-image-mobile.svg' />
                         </div>
 
                   </div>
