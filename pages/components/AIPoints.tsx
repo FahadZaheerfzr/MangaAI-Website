7e24946a9730167a1504ec45f467f5aa7998b3f1
compile-run-ali <aabdullah.bscs20seecs@seecs.edu.pk>
2023-09-05 22:26:39 +0500

    6.. i think last left

--- pages/components/AIPoints.tsx
+++ pages/components/AIPoints.tsx
@@ -1,55 +1,72 @@
-import React from 'react'
+import React from "react";
 
 const AIPoints = () => {
   return (
-    
-<div className='bg-main-gray pb-72 hidden lg:block font-Lato'>
-        <h1 className=' w-fit m-auto pt-[20%]' >
-            <span
-            className="bg-gradient-to-r from-fuchsia-700 via-slate-500 to-green-500 text-transparent bg-clip-text 'containor'>
-            <div className='bg-gray-800 rounded-lg lg:w-1/3 pl-5 justify-center relative z-10 font-custom9"
-            style={{ backgroundImage: 'linear-gradient(to right, #8F41B4, #7D98A6, #50C355)',}}>Manga AI Points
-            </span>
-        </h1>
-        <p  className='  ml-96 text-[#F8F7F5] w-[59%] mt-3'>At Manga AI, we highly value the contributions of our users. In pursuit of this, we're excited to introduce Manga <br/> AI Points – a dynamic system that rewards the creativity and active participation of our MANGA AI Bot users</p>
-       
-        <img className="  w-full h-fit" src="./images/divq1.svg" alt="error" />
+    <div className="bg-main-gray lg:flex lg:flex-col hidden items-center pb-32 pt-56 font-Lato relative gap-10 h-full px-10">
+      <h1 className=" z-10">
+        <span
+          className="bg-gradient-to-r from-fuchsia-700 via-slate-500 to-green-500 text-transparent bg-clip-text
+                bg-gray-800 rounded-lg lg:w-1/3 pl-5 justify-center relative z-10 lg:text-[54px] text-[26px] font-black "
+          style={{
+            backgroundImage:
+              "linear-gradient(to right,#8F41B4, #7D98A6, #50C355)",
+          }}
+        >
+          Manga AI Points
+        </span>
+      </h1>
+      <p className="  text-[#F8F7F5]   z-10">
+        At Manga AI, we highly value the contributions of our users. In pursuit
+        of this, we're excited to introduce Manga <br /> AI Points – a dynamic
+        system that rewards the creativity and active participation of our MANGA
+        AI Bot users
+      </p>
 
+      <img className="  absolute z-0 h-max" src="./images/divq1.svg" alt="error" />
 
-          <div className='mt-[-45%] relative top-0   flex left-0 w-full h-full items-center justify-center pb-24 ' id='containor'>
-                <div className='bg-gray-800 rounded-lg  pl-5 justify-center relative z-10 w-fit p-4 '>
-                    <h1 className='text-[#88C48A] font-custom8  justify-center lg:text-lg text-lg w-36 m-auto text-center pb-4 pt-5'>
-                    How it Works
-                    </h1>
-                    <p className='text-[#F8F7F5] w-64 m-auto text-justify pb-5'>
-                    For every image crafted using the MANGA AI Bot, users earn 1 Manga AI Point. With a daily limit of 3 generated images per user, dedicated enthusiasts can get up to 3 points each day. Points are exclusive to the official MANGA AI group. Only images generated within this group qualify for points.
-                    </p>                   
-                </div>
+      <div
+        className=" relative  flex left-0 w-full items-center justify-center gap-7"
+      >
+        <div className="bg-main-gray-lighter rounded-[28px]  p-5 justify-center relative z-10 w-1/3 flex flex-col gap-8 items-center ">
+          <h1 className="text-[#88C48A] font-custom8  justify-center lg:text-4xl text-lg text-center w-full">
+            How it Works
+          </h1>
+          <p className="text-[#F8F7F5] lg:text-[23px]  text-justify pb-5 px-5">
+            For every image crafted using the MANGA AI Bot, users earn 1 Manga
+            AI Point. With a daily limit of 3 generated images per user,
+            dedicated enthusiasts can get up to 3 points each day. Points are
+            exclusive to the official MANGA AI group. Only images generated
+            within this group qualify for points.
+          </p>
+        </div>
 
-                <div className='bg-gray-800 rounded-lg  pl-5 justify-center float-left  ml-5 relative z-10 w-fit p-4'>
-                    <h1 className='text-[#88C48A] font-custom8  justify-center lg:text-lg text-lg  m-auto text-center pb-4 pt-5'>
-                    Gems of Potential
-                    </h1>
-                    <p className='text-[#F8F7F5] w-64 m-auto text-justify pb-5'>
-                    Imagine Manga AI Points as precious gems you're collecting on your creative journey. These gems hold potential value for future opportunities within the expansive MANGA AI ecosystem. As our platform evolves, these points could unlock diverse, exciting possibilities and rewards.
-                    </p>
-                </div>
+        <div className="bg-main-gray-lighter rounded-[28px]  p-5 justify-center float-left   relative z-10 w-1/3 flex flex-col gap-8 items-center">
+          <h1 className="text-[#88C48A] font-custom8  justify-center lg:text-4xl text-lg text-center  w-full">
+            Gems of Potential
+          </h1>
+          <p className="text-[#F8F7F5] lg:text-[23px]  text-justify pb-5 px-5">
+            Imagine Manga AI Points as precious gems you're collecting on your
+            creative journey. These gems hold potential value for future
+            opportunities within the expansive MANGA AI ecosystem. As our
+            platform evolves, these points could unlock diverse, exciting
+            possibilities and rewards.
+          </p>
+        </div>
 
-                <div className='bg-gray-800 rounded-lg  pl-5 justify-center ml-5 relative z-10 w-fit p-4 '>
-                    <h1 className='text-[#88C48A] font-custom8 justify-center lg:text-lg text-lg w-40 m-auto text-center pb-8 mt-5'>
-                    Your Dedication Rewarded
-                    </h1>
-                    <p className='text-[#F8F7F5] w-64 m-auto text-justify pb-6 '>
-                    Your commitment to the MANGA AI community is immensely valued, and Manga AI Points are our token of appreciation. Keep producing remarkable images, accumulate your points, and anticipate the exciting prospects these gems will unveil.
-                    </p>
-                </div>
+        <div className="bg-main-gray-lighter rounded-[28px]  p-5 justify-center  relative z-10 w-1/3 flex flex-col gap-8 items-center ">
+          <h1 className="text-[#88C48A] font-custom8 justify-center lg:text-4xl text-lg text-center  w-full">
+            Your Dedication Rewarded
+          </h1>
+          <p className="text-[#F8F7F5] lg:text-[23px]  text-justify pb-6 px-6 ">
+            Your commitment to the MANGA AI community is immensely valued, and
+            Manga AI Points are our token of appreciation. Keep producing
+            remarkable images, accumulate your points, and anticipate the
+            exciting prospects these gems will unveil.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-          </div>
-        
-</div>
-
-
-  )
-}
-
-export default AIPoints
+export default AIPoints;
--- pages/components/Partners.tsx
+++ pages/components/Partners.tsx
@@ -1,62 +1,102 @@
-import React from 'react'
+import React from "react";
 
 const Partners = () => {
   return (
-    
-<div className='bg-main-gray-light pb-10 font-Lato'>
-       <h1 className=' w-fit m-auto pt-10 lg:text-4xl font-bold text-xl' >
-                <span
-                className="bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 text-transparent bg-clip-text 'containor'>
-                <div className='bg-gray-800 rounded-lg lg:w-1/3 pl-5 justify-center relative z-10 font-custom9"
-                style={{ backgroundImage: 'linear-gradient(to right, #8F41B4, #7D98A6, #50C355)',}}>Partners and Supporters
-                </span>
-         </h1>
+    <div className="bg-main-gray-light  font-Lato flex flex-col items-center gap-10 p-10 w-screen">
+      <h1 className="lg:text-[54px] text-[26px] font-black ">
+        <span
+          className="bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 text-transparent bg-clip-text 'containor' 
+                  bg-gray-800 rounded-lg  justify-center relative z-10 font-custom9 "
+          style={{
+            backgroundImage:
+              "linear-gradient(to right,#8F41B4, #7D98A6, #50C355)",
+          }}
+        >
+          Partners and Supporters
+        </span>
+      </h1>
 
-            <div className='text-[#F8F7F5] flex lg:space-x-12 space-x-2 mt-5 w-fit m-auto'>
-                    <div className='w-fit'>
-                      <img className='lg:w-16 w-7 justify-center m-auto' src="./images/divt1.svg " alt="error" />
-                      <p className='text-center lg:text-base text-xs pt-3'>Fendi</p>
-                    </div>
+      <div className="text-[#F8F7F5] flex lg:space-x-12 space-x-4 mt-5  uppercase">
+        <div className="flex flex-col gap-2 items-center text-center w-max">
+          <img
+            className="xl:w-32 lg:w-20 w-8 justify-center uppercase"
+            src="./images/divt1.svg "
+            alt="error"
+          />
+          <p className=" lg:text-base text-[4px] ">Fendi</p>
+        </div>
 
-                    <div className='w-fit'>
-                      <img className='lg:w-16 w-7 justify-center m-auto' src="./images/divt2.svg " alt="error" />
-                      <p className='text-center lg:text-base text-xs pt-3'>Mesh.Ai</p>
-                    </div>
+        <div className="flex flex-col gap-2 items-center text-center w-max">
+          <img
+            className="xl:w-32 lg:w-20 w-8 justify-center uppercase"
+            src="./images/divt2.svg "
+            alt="error"
+          />
+          <p className=" lg:text-base text-[4px] ">Mesh.Ai</p>
+        </div>
 
-                    <div className='w-fit'>
-                      <img className='lg:w-16 w-7 justify-center m-auto' src="./images/divt3.svg " alt="error" />
-                      <p className='text-center pt-3 lg:text-base text-xs w-8 lg:w-20'>luffy</p>
-                    </div>
+        <div className="flex flex-col gap-2 items-center text-center w-max">
+          <img
+            className="xl:w-32 lg:w-20 w-8 justify-center uppercase"
+            src="./images/divt3.svg "
+            alt="error"
+          />
+          <p className="  lg:textbasee text-[4px] w-8 lg:w-20">
+            luffy token exchange
+          </p>
+        </div>
 
-                    <div className='w-fit'>
-                      <img className='lg:w-16 w-7 justify-center m-auto' src="./images/divt4.svg " alt="error" />
-                      <p className='text-center lg:text-base text-xs pt-3 '>Fendi</p>
-                    </div>
+        <div className="flex flex-col gap-2 items-center text-center w-max">
+          <img
+            className="xl:w-32 lg:w-20 w-8 justify-center uppercase"
+            src="./images/divt4.svg "
+            alt="error"
+          />
+          <p className=" lg:text-base text-[4px]  ">Fendi</p>
+        </div>
 
-                    <div className='w-fit'>
-                      <img className='lg:w-16 w-7 justify-center m-auto' src="./images/divt5.svg " alt="error" />
-                      <p className='text-center lg:text-base text-xs pt-3'>Mesh.Ai</p>
-                    </div>
+        <div className="flex flex-col gap-2 items-center text-center w-max">
+          <img
+            className="xl:w-32 lg:w-20 w-8 justify-center uppercase"
+            src="./images/divt5.svg "
+            alt="error"
+          />
+          <p className=" lg:text-base text-[4px] ">Mesh.Ai</p>
+        </div>
 
-                    <div className='w-fit'>
-                      <img className='lg:w-16 w-7 justify-center m-auto' src="./images/divt6.svg " alt="error" />
-                      <p className='text-center lg:text-base text-xs pt-3 w-8 lg:w-20 '>luffy</p>
-                    </div>
+        <div className="flex flex-col gap-2 items-center text-center w-max">
+          <img
+            className="xl:w-32 lg:w-20 w-8 justify-center uppercase"
+            src="./images/divt6.svg "
+            alt="error"
+          />
+          <p className=" lg:text-base text-[4px]  w-8 lg:w-20 ">
+            luffy token exchange
+          </p>
+        </div>
 
-                    <div className='w-fit'>
-                      <img className='lg:w-16 w-7 justify-center m-auto' src="./images/divt7.svg " alt="error" />
-                      <p className='text-center lg:text-base text-xs pt-3'>Mesh.Ai</p>
-                    </div>
+        <div className="flex flex-col gap-2 items-center text-center w-max">
+          <img
+            className="xl:w-32 lg:w-20 w-8 justify-center uppercase"
+            src="./images/divt7.svg "
+            alt="error"
+          />
+          <p className=" lg:text-base text-[4px] ">Mesh.Ai</p>
+        </div>
 
-                    <div className='w-fit'>
-                      <img className='lg:w-16 w-7 justify-center m-auto' src="./images/divt8.svg " alt="error" />
-                      <p className='text-center lg:text-base text-xs pt-3 lg:w-20 w-8'>luffy</p>
-                    </div>
-            </div>
+        <div className="flex flex-col gap-2 items-center text-center w-max">
+          <img
+            className="xl:w-32 lg:w-20 w-8 justify-center uppercase"
+            src="./images/divt8.svg "
+            alt="error"
+          />
+          <p className=" lg:text-base text-[4px]  lg:w-20 w-8">
+            luffy token exchange
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-</div>
-
-  )
-}
-
-export default Partners
+export default Partners;
