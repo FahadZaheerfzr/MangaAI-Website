fed6ce7c9bac74e681ae6359173d76df14787515
compile-run-ali <aabdullah.bscs20seecs@seecs.edu.pk>
2023-09-05 21:55:20 +0500

    4 done

--- pages/components/AIComunity.tsx
+++ pages/components/AIComunity.tsx
@@ -1,27 +1,34 @@
-import React from 'react'
-import {FaArrowRight,FaLinkedinIn} from 'react-icons/fa'
+import React from "react";
+import { FaArrowRight, FaLinkedinIn } from "react-icons/fa";
 
 const AIComunity = () => {
   return (
-   
-<div className='bg-main-gray pb-14 font-Lato'>
-    <h1 className=' w-fit m-auto pt-5' >
-                <span
-                className="bg-gradient-to-r from-fuchsia-700 via-slate-500 to-green-500 text-transparent bg-clip-text 'containor'>
-                <div className='bg-gray-800 rounded-lg lg:w-1/3 pl-5 justify-center relative z-10 font-custom9"
-                style={{ backgroundImage: 'linear-gradient(to right,#8F41B4, #7D98A6, #50C355)',}}>Manga AI Community
-                </span>
-     </h1>
-            <p  className='  m-auto text-[#F8F7F5] lg:w-[46%] w-[75%] text-center mt-3 mb-5 lg:text-base text-xs'>Here are beautiful anime images and captivating animated artworks, all created by our skilled community using our AI technology.</p>
-            <div className='w-fit  mb-12'>
-                 <img className='w-[75%]  m-auto' src="./images/divu1.svg" alt="error" />
-            </div>
+    <div className="bg-main-gray flex flex-col py-10 font-Lato items-center gap-10 lg:px-10">
+      <h1 className=" ">
+        <span
+          className="bg-gradient-to-r from-fuchsia-700 via-slate-500 to-green-500 text-transparent bg-clip-text
+                bg-gray-800 rounded-lg lg:w-1/3 pl-5 justify-center relative z-10 lg:text-[54px] text-[26px] font-black "
+          style={{
+            backgroundImage:
+              "linear-gradient(to right,#8F41B4, #7D98A6, #50C355)",
+          }}
+        >
+          Manga AI Community
+        </span>
+      </h1>
+      <p className="  m-auto text-[#F8F7F5] lg:w-[46%] w-[75%] text-center mt-3 mb-5 lg:text-base text-[11px] lg:text-[23px] font-normal leading-[14px]">
+        Here are beautiful anime images and captivating animated artworks, all
+        created by our skilled community using our AI technology.
+      </p>
+      <div className="">
+        <img className="" src="./images/divu1.svg" alt="error" />
+      </div>
 
-            <button className="rounded-md text-[#F8F7F5] bg-gradient-to-r from-fuchsia-700 via-slate-500 to-green-500 p-4 flex m-auto ">Create Your Anime Art Now <p className='ml-6 mt-1'><FaArrowRight/></p></button>
-               
-</div>
+      <button className="rounded-[13px] text-[#F8F7F5] bg-gradient-to-r from-fuchsia-700 via-slate-500 to-green-500 py-1 lg:py-4 flex text-[13px] font-bold leading-[34px] tracking-tight w-max gap-10 items-center px-10 lg:text-[26px]">
+        <p>Create Your Anime Art Now</p> <FaArrowRight />
+      </button>
+    </div>
+  );
+};
 
-  )
-}
-
-export default AIComunity
+export default AIComunity;
