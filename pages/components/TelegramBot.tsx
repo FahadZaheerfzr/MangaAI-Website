7798815db1e815c4fe92b794f10010408371edc5
compile-run-ali <aabdullah.bscs20seecs@seecs.edu.pk>
2023-09-05 23:21:55 +0500

    telegram one

--- pages/components/TelegramBot.tsx
+++ pages/components/TelegramBot.tsx
@@ -1,55 +1,68 @@
-import React from 'react'
+import React from "react";
 
 const TelegramBot = () => {
   return (
-    
+    <div className="bg-main-purple pb-44 lg:pb-56 font-Lato relative flex flex-col items-center gap-10">
+      <img className="absolute pt-11" src="./images/divs1.svg " alt="error" />
+      <p className=" w-fit m-auto pt-24 text-center font-bold lg:text-5xl  text-2xl">
+        <span
+          className="bg-gradient-to-r from-fuchsia-700 via-slate-500 to-green-500 text-transparent bg-clip-text uppercase
+                bg-gray-800 rounded-lg lg:w-1/3 pl-5 justify-center relative z-10 lg:text-[54px] text-[26px] font-black "
+          style={{
+            backgroundImage:
+              "linear-gradient(to right,#8F41B4, #7D98A6, #50C355)",
+          }}
+        >
+          Manga AI telegram bot
+        </span>
+      </p>
+      <p className="text-[#F8F7F5] text-[19px] lg:text-[33px] font-bold uppercase tracking-[13.28px] ">
+        IS LIVE NOW
+      </p>
+      <p className="text-[#F8F7F5] text-center text-[11px] lg:text-[23px] font-medium leading-loose tracking-wide">
+        Make fantastic anime artwork in just seconds with Manga AI's Ai Bot
+      </p>
 
-<div className='bg-main-purple pb-40 font-Lato'>
-          <img className='absolute pt-11' src="./images/divs1.svg " alt="error" />
-          <p className=' w-fit m-auto pt-24 text-center font-bold lg:text-5xl  text-2xl' >
-                <span
-                  className="bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 text-transparent bg-clip-text 'containor'>
-                  <div className='bg-gray-800 rounded-lg lg:w-1/3 pl-5 justify-center relative z-10 font-custom9"
-                  style={{ backgroundImage: 'linear-gradient(to right, #8F41B4, #7D98A6, #50C355)',}}>MANGA AI TELEGRAM BOT
-                  </span>
-                </p>
-                <p className='text-[#F8F7F5] font-semibold text-center tracking-wide mt-6 '>IS LIVE NOW</p>
-                <p className='text-[#F8F7F5] text-center m-auto lg:text-base text-xs w-72 lg:w-full mt-6'>Make fantastic anime artwork in just seconds with Manga AI's Ai Bot</p>
-                
-                <div className='flex pl-20'>
-                    <div className=' items-center  lg:w-96 relative lg:pl-16  mr-1'>
-                            <div >
-                              <img className='w-32 lg:w-44 h-auto' src="./images/divs2.svg" alt="error" />
-                            </div>                             
-                    </div>
-                    
+      <div className="flex gap-20 items-center w-full">
+        <div className=" ">
+          <div>
+            <img
+  className="w-full max-w-full"
+  src="./images/divs2.svg"
+              alt="error"
+            />
+          </div>
+        </div>
 
-                  <div className='bg-[#473059]  rounded-3xl p-2 h-fit mt-[8%] w-48 lg:w-[40%] flex'>
-                    <div className='m-auto flex lg:space-x-40 text-xs lg:text-lg space-x-12 '>
-                        <div className='text-[#F8F7F5] leading-3 text-center  '>
-                            <h3 className='font-bold text-2xl mt-1 mb-2'>22</h3>
-                            <p >Total Users</p>
-                        </div>
-                        <div className='text-[#F8F7F5] text-center leading-3 lg:leading-6 '>
-                            <h3 className='font-bold text-2xl mt-1 mb-1'>500</h3>
-                            <p className='mb-2' >Total images generated</p>
-                        </div>
-                        </div>
-                  </div>
+        <div className="bg-[#473059] rounded-xl lg:rounded-[52px] py-4 lg:py-7 sm:px-10 px-2 md:px-20 lg:px-44  w-max justify-center gap-32 flex ">
+            <div className="text-[#F8F7F5] flex flex-col gap-1 text-center ">
+              <h3 className="font-black text-[32px] lg:text-[50px]">22</h3>
+              <p className="text-[#C28BEC] text-[6px] lg:text-xs font-medium leading-loose tracking-tight">Total Users</p>
+            </div>
+            <div className="text-[#F8F7F5] flex flex-col gap-1 text-center">
+              <h3 className="font-black text-[32px] lg:text-[50px]">500</h3>
+              <p className="text-[#C28BEC] text-[6px] lg:text-xs font-medium leading-loose tracking-tight">Total images generated</p>
+            </div>
+        </div>
 
-                  <div className=' items-center  lg:w-96 relative lg:pl-[10%] ml-1 mr-1'>
-                            <div >
-                              <img className=' w-28 lg:w-40 h-auto' src="./images/divs3.svg" alt="error" />
-                            </div> 
-                    </div>
+        <div className=" items-center   ">
+          <div>
+            <img
+  className="w-full max-w-full"
+  src="./images/divs3.svg"
+              alt="error"
+            />
+          </div>
+        </div>
+      </div>
 
-               </div>
+      <img
+        className="  left-1/2 -translate-x-1/2 md:w-[50%] w-[70%] absolute -bottom-10 lg:-bottom-56 z-20"
+        src="./images/divs4.svg"
+        alt="error"
+      />
+    </div>
+  );
+};
 
-               <img className='w-[40%] ml-[30%] auto absolute' src="./images/divs4.svg" alt="error" />
-
-</div>
-
-  )
-}
-
-export default TelegramBot
+export default TelegramBot;
