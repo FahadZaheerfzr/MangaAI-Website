ab0d5564e207cd76f5f87c7e3e6c26d5e777f7bc
compile-run-ali <aabdullah.bscs20seecs@seecs.edu.pk>
2023-09-26 22:00:28 +0500

    bug fix

--- pages/config/constants.js
+++ pages/config/constants.js
@@ -1,4 +1,4 @@
 const contractAddress = "0x26B4C58DD868CC3Fb6aD27292A9509Af5ce5B9Ca";
-const BACKEND_URL = "http://127.0.0.1:8000/";
+const BACKEND_URL = "https://api.mangaai.org/";
 
 export { contractAddress, BACKEND_URL };
