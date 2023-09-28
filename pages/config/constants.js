3f86e264f37374f6f32bb1735d9854b6ee0c2446
FahadZaheerfzr <fahadzaheer19@gmail.com>
2023-09-28 18:47:04 +0500

    Updated backend url

--- pages/config/constants.js
+++ pages/config/constants.js
@@ -1,4 +1,4 @@
 const contractAddress = "0x26B4C58DD868CC3Fb6aD27292A9509Af5ce5B9Ca";
-const BACKEND_URL = "https://api.mangaai.org/";
+const BACKEND_URL = "http://localhost:8005/";
 
 export { contractAddress, BACKEND_URL };
