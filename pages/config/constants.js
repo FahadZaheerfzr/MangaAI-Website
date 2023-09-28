9940ccec6ccf79cbb4f63fac0fbd3dffd2c4051c
FahadZaheerfzr <fahadzaheer19@gmail.com>
2023-09-28 18:52:02 +0500

    Backend url updated

--- pages/config/constants.js
+++ pages/config/constants.js
@@ -1,4 +1,4 @@
 const contractAddress = "0x26B4C58DD868CC3Fb6aD27292A9509Af5ce5B9Ca";
-const BACKEND_URL = "http://localhost:8005/";
+const BACKEND_URL = "http://127.0.0.1:8005";
 
 export { contractAddress, BACKEND_URL };
